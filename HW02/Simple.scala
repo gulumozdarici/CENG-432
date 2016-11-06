819c9dcbc1eaf7bad4fa6bcb24fa9dab39f8b883
gulumozdarici <gulumozdarici.94@gmail.com>
2016-11-06 22:24:39 +0200

    Add files via upload

---
+++ HW02/Simple.scala
@@ -0,0 +1,16 @@
+//		Gülüm ÖZDARICI
+//			200201003
+
+object Simple extends App{
+  	
+	 	var nut = Array(10, 25, 30)
+	 
+			nut.foreach(i => if (i % 2 == 0){
+          							println(i * 2)
+          						}      
+          
+          					 else println(i * 3) 
+          				)
+}
+
+
