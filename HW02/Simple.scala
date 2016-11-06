37653f5ea60c3807cdbac04fd8b0f1fd148c519e
gulumozdarici <gulumozdarici.94@gmail.com>
2016-11-06 22:25:58 +0200

    Update Simple.scala

--- HW02/Simple.scala
+++ HW02/Simple.scala
@@ -2,15 +2,12 @@
 //			200201003
 
 object Simple extends App{
-  	
 	 	var nut = Array(10, 25, 30)
-	 
 			nut.foreach(i => if (i % 2 == 0){
-          							println(i * 2)
-          						}      
-          
-          					 else println(i * 3) 
-          				)
+          					println(i * 2)
+          				 }      
+          				 else println(i * 3) 
+          			    )
 }
 
 
